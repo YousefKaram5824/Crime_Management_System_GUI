402a58b3a7eaf910ae112dc2c4c78defc06c28a6
youssef karam <1020118@stemegypt.edu.eg>
2024-11-30 10:17:31 +0200

    GUI

---
+++ src/main/java/com/example/crime_management_system_gui/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.crime_management_system_gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
