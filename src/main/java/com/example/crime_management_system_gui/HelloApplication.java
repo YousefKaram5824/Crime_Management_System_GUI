15ac4662a942deacabb190428bebfee446f3fd2e
youssef karam <1020118@stemegypt.edu.eg>
2024-11-30 12:04:46 +0200

    GUI

--- src/main/java/com/example/crime_management_system_gui/HelloApplication.java
+++ src/main/java/com/example/crime_management_system_gui/HelloApplication.java
@@ -1,20 +1,37 @@
 package com.example.crime_management_system_gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 0, 0);
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setFullScreen(true);
         stage.setScene(scene);
+        stage.setFullScreenExitHint("");
         stage.show();
+
+        scene.setOnKeyPressed(event -> {
+            if (Objects.requireNonNull(event.getCode()) == javafx.scene.input.KeyCode.ESCAPE) {
+                Platform.exit();  // Close the application
+            }
+        });
     }
 
     public static void main(String[] args) {
