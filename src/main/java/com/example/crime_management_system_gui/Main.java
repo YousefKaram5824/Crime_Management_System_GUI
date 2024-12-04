8ba052ceaf2faaef9815ddcd775b626e99ff6c83
youssef karam <1020118@stemegypt.edu.eg>
2024-12-04 09:38:46 +0200

    maximum scene

--- src/main/java/com/example/crime_management_system_gui/Main.java
+++ src/main/java/com/example/crime_management_system_gui/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setX(50);
+        stage.setY(0);
         stage.show();
 
         scene.setOnKeyPressed(event -> {
