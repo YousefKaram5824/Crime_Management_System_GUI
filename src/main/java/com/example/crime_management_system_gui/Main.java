420446faa923d572f085621e9c8d6b596ec4a41c
youssef karam <1020118@stemegypt.edu.eg>
2024-12-07 19:55:44 +0200

    icon

--- src/main/java/com/example/crime_management_system_gui/Main.java
+++ src/main/java/com/example/crime_management_system_gui/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,9 +24,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("pages/main.fxml")));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("photos/icon.jpeg")));
+        stage.getIcons().add(icon);
+
+        stage.setTitle("Crime Management System");
         stage.setMaximized(true);
         stage.setResizable(false);
         stage.show();
