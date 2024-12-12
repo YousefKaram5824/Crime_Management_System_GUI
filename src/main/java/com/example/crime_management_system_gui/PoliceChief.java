be6ccaa7a6ce37805fb9f4758bfde7adc0af981d
youssef karam <1020118@stemegypt.edu.eg>
2024-12-12 18:07:30 +0200

    add classes

--- src/main/java/com/example/crime_management_system_gui/PoliceChief.java
+++ src/main/java/com/example/crime_management_system_gui/PoliceChief.java
@@ -7,12 +7,32 @@ import java.io.IOException;
 public class PoliceChief extends Switching {
 
     @FXML
+    private void viewDepartment() throws IOException {
+        switchToPage("view_department.fxml");
+    }
+
+    @FXML
+    private void viewCriminal() throws IOException {
+        switchToPage("view_criminal.fxml");
+    }
+
+    @FXML
+    private void viewOfficer() throws IOException {
+        switchToPage("view_officer.fxml");
+    }
+
+    @FXML
     private void createDepartment() throws IOException {
         switchToPage("create_department.fxml");
     }
 
     @FXML
-    private void viewProfile() throws IOException {
-        switchToPage("view_profile.fxml");
+    private void addCriminal() throws IOException {
+        switchToPage("add_criminal.fxml");
+    }
+
+    @FXML
+    private void assignOfficer() throws IOException {
+        switchToPage("assign_officer.fxml");
     }
 }
